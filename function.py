7c921e4f8c05c218d01d8dcecf32d0330219768c
abc95007 <32200491+abc95007@users.noreply.github.com>
2022-08-12 23:37:53 +0800

    numpy

--- function.py
+++ function.py
@@ -1,9 +1,16 @@
+#%%
+import numpy as np
 def Allen_add(a:int, b:int) -> int:
-    return a + b
+    return a + b 
 
-def YC_checksum(data:list) -> int:
+def YC_checksum(data:np.ndarray) -> int:
     value = 0
     for i in data:
         value = Allen_add(value, i)
     return value
+
+if __name__ == '__main__':
+    data = np.arange(10)
+    checksum = YC_checksum(data)
+    print("checksum:", checksum)  # checksum: 55
 
--- test_func.py
+++ test_func.py
@@ -1,13 +1,11 @@
 #%%
 import function
+import numpy as np
+from raydium import driver
 
-def test_fun():
+def test_Allen_fun():
     assert function.Allen_add(1, 2) == 3
 
-def test_fun2():
-    assert function.YC_checksum([1, 2, 3]) == 6
+def test_YC_fun():
+    assert function.YC_checksum(np.arange(10)) == 45
 
-if __name__ == '__main__':
-    test_fun()
-    test_fun2()
-    print('All tests passed!')
